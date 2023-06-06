058e31c3cce9b78c4400f35fa6dff0a02229eb15
Niclauss Lumoring <103998426+niclausslumoring@users.noreply.github.com>
2023-06-06 13:44:17 +0700

    sms change to login

---
+++ app/src/main/java/com/example/jsteam/activity/core/OTPVerification.java
@@ -0,0 +1,172 @@
+package com.example.jsteam.activity.core;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.jsteam.R;
+import com.example.jsteam.activity.prelogin.MainActivity;
+import com.example.jsteam.activity.prelogin.RegisterActivity;
+
+public class OTPVerification extends AppCompatActivity {
+
+    private EditText otpEt1, otpEt2, otpEt3, otpEt4;
+    private TextView resendBtn;
+
+    private boolean resendEnabled = false;
+    private int resendTime = 60;
+
+    private int selectETPosition = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_otp_verification);
+
+        otpEt1 = findViewById(R.id.otpET1);
+        otpEt2 = findViewById(R.id.otpET2);
+        otpEt3 = findViewById(R.id.otpET3);
+        otpEt4 = findViewById(R.id.otpET4);
+
+        resendBtn = findViewById(R.id.resendBtn);
+
+        final Button verifyBtn = findViewById(R.id.verifyBtn);
+        final TextView otpEmail = findViewById(R.id.otpEmail);
+        final TextView otpMobile = findViewById(R.id.otpMobile);
+
+        final String getEmail = getIntent().getStringExtra("email");
+        final String getMobile = getIntent().getStringExtra("mobile");
+
+        otpEmail.setText(getEmail);
+        otpMobile.setText(getMobile);
+
+        otpEt1.addTextChangedListener(textWatcher);
+        otpEt2.addTextChangedListener(textWatcher);
+        otpEt3.addTextChangedListener(textWatcher);
+        otpEt4.addTextChangedListener(textWatcher);
+
+        showKeyboard(otpEt1);
+
+        startCountDownTimer();
+
+        resendBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(resendEnabled){
+                    startCountDownTimer();
+                }
+            }
+        });
+
+        verifyBtn.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view) {
+                final String generateOtp = otpEt1.getText().toString()+otpEt2.getText().toString()+otpEt3.getText().toString()+otpEt4.getText().toString();
+
+
+                if(generateOtp.equals(getIntent().getStringExtra("otp"))){
+                    Intent intent = new Intent(OTPVerification.this, HomePageActivity.class);
+                    startActivity(intent);
+                }
+                else{
+                    Toast.makeText(OTPVerification.this, "Wrong OTP", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    private void showKeyboard(EditText otpET){
+        otpET.requestFocus();
+        InputMethodManager inputMethodManager = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+        inputMethodManager.showSoftInput(otpET, InputMethodManager.SHOW_IMPLICIT);
+
+    }
+
+    private void startCountDownTimer(){
+        resendEnabled = false;
+        resendBtn.setTextColor(Color.parseColor("#99000000"));
+
+        new CountDownTimer(resendTime * 60, 100){
+
+
+            @Override
+            public void onTick(long l) {
+                resendBtn.setText("resent Code ("+(l/60)+")");
+            }
+
+            @Override
+            public void onFinish() {
+                resendEnabled = true;
+                resendBtn.setText("Resend Code");
+                resendBtn.setTextColor(getResources().getColor(com.google.android.material.R.color.design_default_color_primary));
+            }
+        }.start();
+    }
+    private final TextWatcher textWatcher = new TextWatcher() {
+        @Override
+        public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+        }
+
+        @Override
+        public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+        }
+
+        @Override
+        public void afterTextChanged(Editable editable) {
+            if(editable.length() > 0){
+                if(selectETPosition == 0){
+                    selectETPosition = 1;
+                    showKeyboard(otpEt2);
+                }
+                else if(selectETPosition == 1){
+                    selectETPosition = 2;
+                    showKeyboard(otpEt3);
+                }
+                else if(selectETPosition == 2){
+                    selectETPosition = 3;
+                    showKeyboard(otpEt4);
+                }
+            }
+        }
+    };
+
+    @Override
+    public boolean onKeyUp(int keyCode, KeyEvent event) {
+        if(keyCode == KeyEvent.KEYCODE_DEL){
+            if(selectETPosition == 3){
+                selectETPosition=2;
+                showKeyboard(otpEt1);
+            }
+            else if(selectETPosition == 2){
+                selectETPosition=1;
+                showKeyboard(otpEt2);
+            }
+            else if(selectETPosition == 1){
+                selectETPosition=0;
+                showKeyboard(otpEt3);
+            }
+
+            return true;
+        }
+
+        else{
+            return super.onKeyUp(keyCode, event);
+        }
+    }
+}
--- app/src/main/java/com/example/jsteam/activity/prelogin/MainActivity.java
+++ app/src/main/java/com/example/jsteam/activity/prelogin/MainActivity.java
@@ -1,20 +1,31 @@
 package com.example.jsteam.activity.prelogin;
 
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.telephony.SmsManager;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
 import com.example.jsteam.R;
 import com.example.jsteam.activity.core.HomePageActivity;
+import com.example.jsteam.activity.core.OTPVerification;
+import com.example.jsteam.deprecated.DatabaseConfiguration;
 import com.example.jsteam.helper.UserHelper;
 import com.example.jsteam.model.dao.User;
 
+import java.util.ArrayList;
 import java.util.Objects;
+import java.util.Random;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * @author kareltan
@@ -28,10 +39,69 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         init();
+
+//        Button registerButton = findViewById(R.id.button_login_login_page);
+//        registerButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                if(ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.SEND_SMS) == PackageManager.PERMISSION_GRANTED){
+//                    userHelper.open();
+//                    EditText usernameLoginPage = findViewById(R.id.pt_username_login);
+//                    User user = userHelper.findUser(String.valueOf(usernameLoginPage.getText().toString()));
+//                    SmsManager smsManager = SmsManager.getDefault();
+//                    Toast.makeText(MainActivity.this, user.getUsername(), Toast.LENGTH_SHORT).show();
+//                    Random random = new Random();
+//                    int randomNumber = random.nextInt(10);
+//                    int randomNumber2 = random.nextInt(10);
+//                    int randomNumber3 = random.nextInt(10);
+//                    int randomNumber4 = random.nextInt(10);
+//                    String otp = Integer.toString(randomNumber) + Integer.toString(randomNumber2)+Integer.toString(randomNumber3
+//                    )+Integer.toString(randomNumber4);
+//                    ArrayList<String> parts = smsManager.divideMessage(otp + " is the otp");
+//                    String phone = user.getPhoneNumber();
+//                    String email = user.getEmail();
+//
+//                    smsManager.sendMultipartTextMessage(phone, null, parts, null, null);
+//
+//                    Intent intent = new Intent(MainActivity.this, OTPVerification.class);
+//                    intent.putExtra("mobile", phone);
+//                    intent.putExtra("email", email);
+//                    intent.putExtra("otp", otp);
+//                    startActivity(intent);
+//                }else{
+//                    Toast.makeText(MainActivity.this, "ke otp else", Toast.LENGTH_SHORT).show();
+//                    ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.SEND_SMS}, 100);
+//                }
+//            }
+//        });
     }
+
+//    @Override
+//    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+//        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+//        if (requestCode == 1) {
+//            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+//                EditText phoneRegisterPage = findViewById(R.id.pt_phone_number_register);
+//                SmsManager smsManager = SmsManager.getDefault();
+//                Random random = new Random();
+//                int randomNumber = random.nextInt(10);
+//                int randomNumber2 = random.nextInt(10);
+//                int randomNumber3 = random.nextInt(10);
+//                int randomNumber4 = random.nextInt(10);
+//                String otp = Integer.toString(randomNumber) + Integer.toString(randomNumber2)+Integer.toString(randomNumber3
+//                )+Integer.toString(randomNumber4);
+//                ArrayList<String> parts = smsManager.divideMessage(otp + " is the otp");
+//                String phoneNumber = phoneRegisterPage.getText().toString();
+//                smsManager.sendMultipartTextMessage(phoneNumber, null, parts, null, null);
+//            } else {
+//                Toast.makeText(this, "Permission Denied", Toast.LENGTH_SHORT).show();
+//            }
+//        }
+//    }
 
     private void init(){
         userHelper.open();
+        AtomicInteger index = new AtomicInteger();
         Button loginButton = findViewById(R.id.button_login_login_page);
         TextView notHaveAccountText = findViewById(R.id.tv_didnt_have_account);
         EditText usernameLoginPage = findViewById(R.id.pt_username_login);
@@ -72,9 +142,34 @@ public class MainActivity extends AppCompatActivity {
             }
             else {
                 Toast.makeText(MainActivity.this, "Successfully Login!", Toast.LENGTH_SHORT).show();
-                Intent intentHome = new Intent(MainActivity.this, HomePageActivity.class);
-                intentHome.putExtra("username", user.getUsername());
-                startActivity(intentHome);
+                if(ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.SEND_SMS) == PackageManager.PERMISSION_GRANTED){
+                    userHelper.open();
+                    EditText usernameLoginPage = findViewById(R.id.pt_username_login);
+                    User user2 = userHelper.findUser(String.valueOf(usernameLoginPage.getText().toString()));
+                    SmsManager smsManager = SmsManager.getDefault();
+                    Toast.makeText(MainActivity.this, user.getUsername(), Toast.LENGTH_SHORT).show();
+                    Random random = new Random();
+                    int randomNumber = random.nextInt(10);
+                    int randomNumber2 = random.nextInt(10);
+                    int randomNumber3 = random.nextInt(10);
+                    int randomNumber4 = random.nextInt(10);
+                    String otp = Integer.toString(randomNumber) + Integer.toString(randomNumber2)+Integer.toString(randomNumber3
+                    )+Integer.toString(randomNumber4);
+                    ArrayList<String> parts = smsManager.divideMessage(otp + " is the otp");
+                    String phone = user2.getPhoneNumber();
+                    String email = user2.getEmail();
+
+                    smsManager.sendMultipartTextMessage(phone, null, parts, null, null);
+
+                    Intent intent = new Intent(MainActivity.this, OTPVerification.class);
+                    intent.putExtra("mobile", phone);
+                    intent.putExtra("email", email);
+                    intent.putExtra("otp", otp);
+                    startActivity(intent);
+                }else{
+                    Toast.makeText(MainActivity.this, "ke otp else", Toast.LENGTH_SHORT).show();
+                    ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.SEND_SMS}, 100);
+                }
                 return true;
             }
         }
